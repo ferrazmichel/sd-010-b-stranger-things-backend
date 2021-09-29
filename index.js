6ce2b8ad2abc7ce85daa7891e840e7ad45af36dc
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-09-29 11:14:17 -0300

    trocando os valores padrão por variáveis de ambiente

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 const { PORT, UPSIDEDOWN_MODE } = process.env;
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = Boolean(UPSIDEDOWN_MODE);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
