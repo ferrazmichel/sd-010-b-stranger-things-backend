5e36e44663a5ca3cbf266c2c91454c028f1cba71
Leandro Reis <lleandrofr@gmail.com>
2021-09-29 13:24:36 -0300

    build: configre env variables
    Configure PORT and UPSIDEDOWN_MODE variables for to deploy on heroku
    
    Conclude requirement 1

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use(cors());
 
 const hereIsTheUpsideDown = true;
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
@@ -27,6 +29,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Online at port 3000');
+app.listen(PORT, () => {
+  console.log(`Online at port ${PORT}`);
 });
