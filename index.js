ba007b768f3a7c4771c85c1306d6eae53b85c896
isTorres <ismaeltorres612@gmail.com>
2021-09-29 20:16:17 -0300

    refactored

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE === 'true' || false;
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
