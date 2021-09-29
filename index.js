550a0d6378c76b78533466b43422dd73a137742e
Misael Bueno <misa.hbueno@gmail.com>
2021-09-29 19:30:47 -0300

    change upsidedown again

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-// const hereIsTheUpsideDown = (process.env.UPSIDEDOWN_MODE.toLowerCase() === 'true');
-const hereIsTheUpsideDown = Boolean(process.env.UPSIDEDOWN_MODE);
+const hereIsTheUpsideDown = (process.env.UPSIDEDOWN_MODE.toLowerCase() === 'true');
+// const hereIsTheUpsideDown = Boolean(process.env.UPSIDEDOWN_MODE);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
