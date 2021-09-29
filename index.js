8d20711380c57f8183b21221201d1bdcd667057d
hugolfsimoes <hugolfsimoes@gmail.com>
2021-09-29 20:54:11 -0300

    subindo aplicaçao heroku

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+const { UPSIDEDOWN_MODE } = process.env;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -20,7 +21,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
