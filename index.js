c903d80d512aa02045ba125da4578c66e7667697
tonpasso <wellpasso@gmail.com>
2021-09-29 15:54:11 -0300

    requisito 01

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -16,7 +17,13 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+let hereIsTheUpsideDown;
+
+ if (process.env.UPSIDEDOWN_MODE === 'true') {
+  hereIsTheUpsideDown = true;
+ } else {
+  hereIsTheUpsideDown = false;
+ }
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +34,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Escutando na porta 3000');
 });
