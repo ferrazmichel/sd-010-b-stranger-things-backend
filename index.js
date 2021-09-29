b1bd2c12077c7c9aa6841a9a92f065efeecff4f8
Thiago Kachinsky <tjk091@gmail.com>
2021-09-29 16:54:24 -0300

    Config variaveis de ambiente back

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 require('dotenv').config();
+const cors = require('cors');
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -17,7 +17,9 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+const { PORT, UPSIDEDOWN_MODE } = process.env;
+
+const hereIsTheUpsideDown = (UPSIDEDOWN_MODE === 'true');
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,8 +29,6 @@ app.get('/', (req, res) => {
 
   res.status(200).json(characters);
 });
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
