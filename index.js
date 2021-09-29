40e1642720df6411f70aa8d7d97cf0cbca6dc2b1
hugolfsimoes <hugolfsimoes@gmail.com>
2021-09-29 18:42:51 -0300

    - [x] 6 - Verifica as variáveis de ambiente - [x] 4 - Verifica se o módulo pm2 foi instalado no projeto

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -17,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -28,6 +29,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
