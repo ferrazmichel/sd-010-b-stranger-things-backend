2a9c091beac6f472949b7ecac7bdf2047e89c0bb
RafaMaths <rmathscoach@gmail.com>
2021-09-29 20:43:24 -0300

    fix port

--- index.js
+++ index.js
@@ -30,5 +30,5 @@ app.get('/', (req, res) => {
 const { PORT } = process.env;
 
 app.listen(PORT, () => {
-  console.log('Escutando na porta 3000');
+  console.log(`Escutando na porta ${PORT}`);
 });
