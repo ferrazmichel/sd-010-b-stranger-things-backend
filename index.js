d6f0246283972f6b8fffc121182f97db69b98943
Daniel <dsouzaborini@gmail.com>
2021-09-29 16:15:43 -0300

    add variaveis de ambiente

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const strangerThingsService = new StrangerThingsService(
   strangerThingsRepository,
 );
 
+const { PORT } = process.env;
+
 app.use(cors());
 
 const hereIsTheUpsideDown = true;
@@ -27,8 +29,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
 
 // :D
