4e5221b1ad6d72c220e785f99e9348a1c8f372f9
Caio <thehaseol@gmail.com>
2021-09-29 16:08:10 -0300

    Fix Port

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Escutando na porta ${PORT}`);
 });
