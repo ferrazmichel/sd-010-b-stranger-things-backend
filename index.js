dae778f394c57a21354a80284ff2cdeda1a4a9fe
legalvao1 <legalvao1@gmail.com>
2021-09-29 17:48:10 -0300

    setando variaveis de ambiente

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 const PORT = process.env.PORT || '3000';
-const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE || true;
 
-const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+console.log(hereIsTheUpsideDown);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
