35b57e68b1fde17bd13a8ffea5e06ce58e2326b9
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-09-29 15:21:27 -0300

    req 1 a 5 - concluído

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 
+require('dotenv').config();
+
+const { PORT, UPSIDEDOWN_MODE } = process.env;
+
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -16,17 +20,17 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+// const hereIsTheUpsideDown = true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    Boolean(UPSIDEDOWN_MODE),
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
