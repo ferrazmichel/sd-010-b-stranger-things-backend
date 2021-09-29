381c25b24f58b644dc8a3ff21e5edf464cb21e43
Caio <thehaseol@gmail.com>
2021-09-29 16:23:45 -0300

    fix

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
-const { PORT, UPSIDEDOWN_MODE } = process.env;
-const hereIsTheUpsideDown = /^true$/.test(UPSIDEDOWN_MODE);
-
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
 );
@@ -19,6 +16,8 @@ const strangerThingsService = new StrangerThingsService(
 );
 
 app.use(cors());
+
+const hereIsTheUpsideDown = JSON.parse(process.env.UPSIDEDOWN_MODE) || true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -29,6 +28,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT || 3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
 });
