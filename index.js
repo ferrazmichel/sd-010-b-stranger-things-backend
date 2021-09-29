b54502609d66f19d63239c4a360e894c6ded7c8f
Arlen Freitas <arlenfreitas1@gmail.com>
2021-09-29 18:38:35 -0300

    add dotenv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv').config();
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -14,7 +14,7 @@ const strangerThingsService = new StrangerThingsService(
   strangerThingsRepository,
 );
 
-const PORT = process.env.PORT || 3000;
+const { PORT } = process.env;
 
 app.use(cors());
 
