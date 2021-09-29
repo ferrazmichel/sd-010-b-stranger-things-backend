3fd3c741e2619341ea9242bebed79e7d61bfce9a
Fabio Marturano <fabiocmjor@gmail.com>
2021-09-29 16:27:31 -0300

    ajuste hereIsTheUpsideDown

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-const UPSIDEDOWN_MODE = !!process.env.UPSIDEDOWN_MODE || false;
+const { UPSIDEDOWN_MODE } = process.env;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -20,7 +20,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -28,6 +28,7 @@ app.get('/', (req, res) => {
     hereIsTheUpsideDown,
   );
 
+  console.log(typeof hereIsTheUpsideDown, hereIsTheUpsideDown);
   res.status(200).json(characters);
 });
 
