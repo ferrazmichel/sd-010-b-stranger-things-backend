e9715b37d163123f7a96ab0a68f8a44cb3abc527
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-09-29 19:13:55 -0300

    change type of environment variable

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
+const envVariable = UPSIDEDOWN_MODE;
+
+const hereIsTheUpsideDown = (envVariable === 'true'); 
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
