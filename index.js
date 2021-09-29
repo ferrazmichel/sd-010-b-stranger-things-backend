ce1ba4e1b8ae0b2605c1468f2621ffae17c4d29b
isTorres <ismaeltorres612@gmail.com>
2021-09-29 18:20:57 -0300

    PORT deploy

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.get('/', (req, res) => {
 });
 
 const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE === 'true' || false;
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT
 
-app.listen(port, () => {
-  console.log(`Escutando na porta ${port}`);
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
