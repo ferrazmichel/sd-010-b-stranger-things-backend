ab57166c29080ded46f2779ad77d8dfe80544165
André Arnoni <andre_arnoni@yahoo.com.br>
2021-09-30 14:09:13 -0300

    executado requisito 5

--- tests/procfile.test.js
+++ tests/procfile.test.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 
-describe.skip('5 - Verifica a configuração do arquivo Procfile', () => {
+describe('5 - Verifica a configuração do arquivo Procfile', () => {
   const [type, script] = fs.readFileSync('Procfile', 'utf8').split(':');
 
-  it.skip('Será validado que o dyno é do tipo web.', () => {
+  it('Será validado que o dyno é do tipo web.', () => {
     expect(type).toEqual('web');
   });
 
-  it.skip('Será validado que o script inicia o server com pm2 e se usa o ecosystem.', () => {
+  it('Será validado que o script inicia o server com pm2 e se usa o ecosystem.', () => {
     const scriptParts = script.trim().split(' ');
     expect(scriptParts[0]).toMatch(/^pm2$|^pm2-/);
     expect(scriptParts).toContain('start');
