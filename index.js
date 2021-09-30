aec0f82cf952afcebf790d9a2765822e992fdc34
Diego Serafim <diegoserafim1@gmail.com>
2021-09-30 18:01:54 -0300

    instalado o pm2

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv').config();
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
-
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
 );
@@ -16,7 +15,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -26,7 +25,8 @@ app.get('/', (req, res) => {
 
   res.status(200).json(characters);
 });
+const { PORT } = process.env;
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log('Escutando na porta 3000', `${PORT}`);
 });
