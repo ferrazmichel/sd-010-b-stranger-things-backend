333890cf0d949b271ce5a1dbe336317d94588507
André Arnoni <andre_arnoni@yahoo.com.br>
2021-09-30 13:24:23 -0300

    executados requisitos 1 e 2

--- tests/package.test.js
+++ tests/package.test.js
@@ -1,7 +1,7 @@
 const { scripts } = require('../package.json');
 
 describe('3 - Verifica se os scripts do package.json estão corretos', () => {
-  it('Será validado que o comando `start` inicia o server com pm2 e se usa o ecosystem.', () => {
+  it.skip('Será validado que o comando `start` inicia o server com pm2 e se usa o ecosystem.', () => {
     const scriptParts = scripts.start.split(' ');
     expect(scripts).toHaveProperty('start');
     expect(scriptParts[0]).toMatch(/^pm2$|^pm2-/);
@@ -9,7 +9,7 @@ describe('3 - Verifica se os scripts do package.json estão corretos', () => {
     expect(scriptParts).toContain('ecosystem.config.yml');
   });
 
-  it('Será validado que o comando `start:dev` inicia o server com pm2, se não usa o ecosystem e abre em watchMode.', () => {
+  it.skip('Será validado que o comando `start:dev` inicia o server com pm2, se não usa o ecosystem e abre em watchMode.', () => {
     const scriptParts = scripts['start:dev'].split(' ');
     expect(scripts).toHaveProperty('start:dev');
     expect(scriptParts[0]).toMatch(/^pm2$|^pm2-/);
