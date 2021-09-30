66f346edd7d3f07384aa2697b12c040f3ce82074
André Arnoni <andre_arnoni@yahoo.com.br>
2021-09-30 18:15:35 -0300

    alterada a porta

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
-const { UPSIDEDOWN_MODE } = process.env;
-const PORT = process.env.PORT || 3000;
+const { PORT, UPSIDEDOWN_MODE } = process.env;
+const port = process.env.PORT || PORT;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -28,6 +28,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(PORT, () => {
-  console.log(`Escutando na porta ${PORT}`);
+app.listen(port, () => {
+  console.log(`Escutando na porta ${port}`);
 });
