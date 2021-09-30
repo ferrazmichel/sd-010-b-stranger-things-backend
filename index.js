28d161d9d07a45e99f54c1dea3fefd417a1c54f9
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-09-29 21:52:44 -0300

    FIXED: parsing error

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const PORT = process.env.PORT || 3000;
-const UPSIDEDOWN_MODE = JSON.parse(process.env.UPSIDEDOWN_MODE);
+const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE === 'true';
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(
