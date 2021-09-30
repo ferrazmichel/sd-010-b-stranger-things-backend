cbcc0235564677de0e3bd2edb5bb3967d18b603b
Jonathan Souza <souzajonathan570@gmail.com>
2021-09-30 11:16:34 -0300

    convertendo variavel de ambiente

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
-
 app.get('/', (req, res) => {
+  console.log(JSON.parse(hereIsTheUpsideDown));
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    JSON.parse(hereIsTheUpsideDown),
   );
 
   res.status(200).json(characters);
