b5e3d5f0b5ebf6162a334d38f61b0ca74107e654
André Arnoni <andre_arnoni@yahoo.com.br>
2021-09-30 13:44:03 -0300

    executado requisito 3

--- tests/ecosystem.test.js
+++ tests/ecosystem.test.js
@@ -1,34 +1,34 @@
 const fs = require('fs');
 const yml = require('js-yaml');
 
-describe.skip('2 - Verifica a configuração do ecosystem.config.yml', () => {
+describe('2 - Verifica a configuração do ecosystem.config.yml', () => {
   const ecosystem = yml.safeLoad(fs.readFileSync('ecosystem.config.yml', 'utf8'));
   const config = ecosystem.apps[0];
 
-  it.skip('Será validado que o ecosystem tem a propriedade name', () => {
+  it('Será validado que o ecosystem tem a propriedade name', () => {
     expect(config).toHaveProperty('name');
   });
 
-  it.skip('Será validado se o script a ser executado é o index.js', () => {
+  it('Será validado se o script a ser executado é o index.js', () => {
     expect(config).toHaveProperty('script');
     expect(config.script).toEqual('./index.js');
   });
 
-  it.skip('Será validado que o modo de execução está configurado para cluster', () => {
+  it('Será validado que o modo de execução está configurado para cluster', () => {
     expect(config).toHaveProperty('exec_mode');
     expect(config.exec_mode).toEqual('cluster');
   });
 
-  it.skip('Será validado que o número de instâncias está definido como 2.', () => {
+  it('Será validado que o número de instâncias está definido como 2.', () => {
     expect(config).toHaveProperty('instances');
     expect(config.instances).toEqual(2);
   });
 
-  it.skip('Será validado que o modo watch está configurado para estar desativado.', () => {
+  it('Será validado que o modo watch está configurado para estar desativado.', () => {
     expect(config.watch).not.toBeTruthy();
   });
 
-  it.skip('Será validado que a reiniciação de memória máxima esta configurada como 200M', () => {
+  it('Será validado que a reiniciação de memória máxima esta configurada como 200M', () => {
     expect(config).toHaveProperty('max_memory_restart');
     expect(config.max_memory_restart).toEqual('200M');
   });
