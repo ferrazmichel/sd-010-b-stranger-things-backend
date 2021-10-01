0757fa84508d2785ca751e58ecbe8013592022ea
Diego Serafim <diegoserafim1@gmail.com>
2021-10-01 16:45:10 -0300

    criado o dotenv no index

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config(); 
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -28,5 +29,5 @@ app.get('/', (req, res) => {
 const { PORT } = process.env;
 
 app.listen(PORT, () => {
-  console.log('Escutando na porta 3000', `${PORT}`);
+  console.log('Escutando na porta 3000');
 });
