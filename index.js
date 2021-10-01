e824c18b641eaf6591598bf421e60c9f019c836a
CIGR2021 <appencigr@gmail.com>
2021-10-01 14:23:52 -0300

    alterei a variável de ambiente UPSIDEDOW_MODE

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const { PORT, UPSIDEDOWN_MODE } = process.env;
+const { PORT } = process.env;
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -18,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
