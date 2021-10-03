534b6b269b5fd464d1e3a5d9275c9b3e3693a5a9
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-10-03 08:55:48 -0300

    Requisito 2 atualizado

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'true';
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'true'  bv:v;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
