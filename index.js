6818b1ea482d74301dc370424d8204e67649d78e
aythan azevedo <aythan16@gmail.com>
2021-10-04 15:54:50 -0300

    exc require doc

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 
-require('dotenv').config();
-
 const { UPSIDEDOWN_MODE } = process.env;
 const PORT = process.env.PORT || 3000;
 
