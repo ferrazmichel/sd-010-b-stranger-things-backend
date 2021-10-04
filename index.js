906668990a2932fddfe26f0a6ce6e22ca6ad9050
Fernanda Porto Oliveira <fernanda.oliveira.mec@gmail.com>
2021-10-04 19:48:12 -0300

    Alterando para mode upsidedown

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const strangerThingsRepository = new StrangerThingsRepository(
     app.use(cors());
     
     const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
-    console.log(typeof hereIsTheUpsideDown);
     app.get('/', (req, res) => {
       const characters = strangerThingsService.search(
         req.query,
