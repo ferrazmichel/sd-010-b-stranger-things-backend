14f5fcc2807cf4a682d2b7e6b4b60416c9bcc24c
Roger Franco <rogerfrancogalt@gmail.com>
2021-10-04 16:13:41 -0400

    deploy

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.use(cors());
 
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'false';
 
-
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
