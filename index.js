214938735673cea33240e47dd03296211d31d8f4
Pablo Fernandes <pabloprates@gmail.com>
2021-10-05 09:43:15 -0300

    Initial data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
+require('dotenv/config');
 
 const app = express();
 
@@ -16,7 +17,9 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const convertStringToBoolean = (text) => (text === 'true');
+
+const hereIsTheUpsideDown = convertStringToBoolean(process.env.UPSIDEDOWN_MODE) || false;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +30,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
